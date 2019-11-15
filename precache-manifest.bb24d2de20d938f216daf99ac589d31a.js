cf2ca1f0eef4a3637f5c9b70c652ded82577a28e
Shubham Jaswal <shubhamjaswal772@gmail.com>
2019-11-15 18:24:24 +0530

    Updates

---
+++ precache-manifest.bb24d2de20d938f216daf99ac589d31a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2f9784ff164ce7820b34b334d793c11",
+    "url": "/placement-socket/index.html"
+  },
+  {
+    "revision": "df34f915515116ce7f15",
+    "url": "/placement-socket/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "e50cf543bb3de2b330f2",
+    "url": "/placement-socket/static/js/2.a73a1c05.chunk.js"
+  },
+  {
+    "revision": "df34f915515116ce7f15",
+    "url": "/placement-socket/static/js/main.29023f73.chunk.js"
+  },
+  {
+    "revision": "60a3dbe0893305982c24",
+    "url": "/placement-socket/static/js/runtime-main.a0843cfa.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/placement-socket/static/media/logo.25bf045c.svg"
+  }
+]);
