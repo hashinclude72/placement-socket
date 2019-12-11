e77b4a5aea23cc0ad737577c46702d80891cc559
tanmay agarwal <tanmayag8958@gmail.com>
2019-12-12 02:24:55 +0530

    register log api n table

---
+++ serverless-apis/src/registerLogs.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-dependencies
+const uuid = require('uuid');
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+module.exports.create = (event, context, callback) => {
+    const timestamp = new Date().getTime();
+    console.error(event.body);
+    const data = JSON.parse(event.body);
+    if (typeof data !== 'object') {
+        console.error('Validation Failed');
+        callback(null, {
+            statusCode: 400,
+            headers: { 'Content-Type': 'text/plain' },
+            body: 'Couldn\'t create the register log.',
+        });
+        return;
+    }
+
+    const params = {
+        TableName: "PlacementSocketRegisterLogs",
+        Item: {
+            id: uuid.v1(),
+            "companyId": data.firstname,
+            "studentSapId": data.lastname,
+            createdAt: timestamp,
+            updatedAt: timestamp
+        },
+    };
+
+    // write the todo to the database
+    dynamoDb.put(params, (error) => {
+        // handle potential errors
+        if (error) {
+            console.error(error);
+            callback(null, {
+                statusCode: error.statusCode || 501,
+                headers: { 'Content-Type': 'text/plain' },
+                body: 'Couldn\'t create the register log, object.',
+            });
+            return;
+        }
+
+        // create a response
+        const response = {
+            statusCode: 200,
+            body: JSON.stringify(params.Item),
+            headers: { 'Content-Type': 'text/plain', 'Access-Control-Allow-Origin': "*" }
+        };
+        callback(null, response);
+    });
+};
